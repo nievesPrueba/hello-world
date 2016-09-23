1cd56b879397e8ddd49bb004dbcfb47adff8c80d
mdlncaracena <mdlncaracena@ITEM-S32672.dhcp.alca.es.sopra>
2016-09-23 09:19:42 +0200

    .travis.yml: modificada la configuración. pom.xml: eliminado plugin assembly. test_prueba.java: eliminada la etiqueta @test de la clase principal.

--- src/test/java/asv/tarificador/test/test_prueba.java
+++ src/test/java/asv/tarificador/test/test_prueba.java
@@ -11,7 +11,10 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 
